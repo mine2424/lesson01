0db22ddd77d6d66efc9ef76160d9ca73e63e093f
mine0215 <minewest4320@outlook.jp>
2020-06-17 23:01:36 +0900

    2-4コマンドライン引数で受け取った数字を季節で表示するやつを実装しました（修正版）

--- 2-4/main.js
+++ 2-4/main.js
@@ -1,11 +1,19 @@
 num = Number(process.argv[2])
 
-if (num >= 2 && num <= 4) {
-    console.log("春")
-} else if (num >= 5 && num <= 7) {
-    console.log("夏")
-} else if (num >= 8 && num <= 10) {
-    console.log("秋")
-} else {
-    console.log("冬")
+switch (true) {
+    case num >= 2 && num <= 4:
+        console.log("春")
+        break;
+
+    case num >= 5 && num <= 7:
+        console.log("夏")
+        break;
+
+    case num >= 8 && num <= 10:
+        console.log("秋")
+        break;
+
+    default:
+        console.log("冬")
+        break;
 }
