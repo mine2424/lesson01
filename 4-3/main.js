7a294d0d2f0ae5213f7b031cd685c890571e444e
mine0215 <minewest4320@outlook.jp>
2020-06-21 15:25:58 +0900

    4-3 エラーログの抽出するやつを実装しました

--- 4-3/main.js
+++ 4-3/main.js
@@ -3,3 +3,9 @@ const errorLogs = [
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+
+for (i = 0; i < errorLogs.length; i++) {
+  const newArr = errorLogs[i].split(/[:,-]/)
+  console.log(newArr[1])
+
+}
