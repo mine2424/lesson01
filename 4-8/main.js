38be84e04e23e46f10386a14cc78e19a58ab3a15
mine0215 <minewest4320@outlook.jp>
2020-06-21 15:37:41 +0900

    4-8 検索のやつを実装しました

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,26 @@
 const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+const search = "こはる"
+const search02 = "はるこ"
+let koharuCounts = 0
+let harukoCounts = 0
+
+function isSearch(words, roop, num) {
+    for (let i = 0; i < num.length; i++) {
+        if (words[roop+i] !== num[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for (let i = 0; i < str.length; i++) {
+    if (isSearch(str,i,search)) {
+        koharuCounts++
+    }
+    if (isSearch(str,i,search02)) {
+        harukoCounts++
+    }
+}
+
+console.log(`こはる : ${koharuCounts}`)
+console.log(`はるこ : ${harukoCounts}`)
