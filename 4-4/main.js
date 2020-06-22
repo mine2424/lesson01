85f191a2f14a57867c3979c50ce81da4db97d842
mine0215 <minewest4320@outlook.jp>
2020-06-22 17:06:50 +0900

    no message

--- 4-4/main.js
+++ 4-4/main.js
@@ -3,12 +3,13 @@ const newArr = []
 
 for (i = 0; i < 10; i++) {
     while (true) {
-        var randomNum = Math.floor(Math.random() * numArr.length);
+        const randomNum = Math.floor(Math.random() * numArr.length);
         if (!newArr.includes(randomNum)) {
             newArr.push(randomNum)
             break;
         }
     }
+
 }
 
 console.log(newArr.join(' '))
