f18883cf6393a4fa2760a8fde0827b5b3e0baa1e
mine0215 <minewest4320@outlook.jp>
2020-06-23 20:43:58 +0900

    4-5 小さい順に並びかえを実装しました　改訂版

--- 4-5/main.js
+++ 4-5/main.js
@@ -1,16 +1,12 @@
-const array = []
 const result = []
+const number = process.argv.slice(2)
 
-for (let i = 2; i < process.argv.length; i++) {
-    array.push(Number(process.argv[i]))
-}
-
-//onsole.log(array)
-
+//0から9999までnumberと総当たりする
 for (a = 0; a < 9999; a++) {
-    for (b = 0; b < array.length; b++) {
-        if (a == array[b]) {
-            result.push(array[b])
+    for (b = 0; b < number.length; b++) {
+        //一致したら新配列にpushする
+        if (a == number[b]) {
+            result.push(number[b])
         }
     }
 }
